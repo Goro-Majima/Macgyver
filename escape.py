9b08cbf5870f7eda2c5bd317d13212a37fbe7e52
Jiomod <lymickael91@gmail.com>
2018-12-26 19:00:47 +0100

    ajout escape

--- escape.py
+++ escape.py
@@ -1,5 +1,31 @@
 import pygame
 from pygame.locals import *
 
+from constantes import *
+
 pygame.init()
 
+fenetre = pygame.display.set_mode((cote_fenetre, cote_fenetre))
+
+#Titre
+pygame.display.set_caption(titre_fenetre)
+
+
+
+
+continuer = 1
+
+while continuer:
+
+	accueil = pygame.image.load(image_accueil).convert()
+	fenetre.blit(accueil, (0,0))
+
+	#Rafraichissement
+	pygame.display.flip()
+	
+	for event in pygame.event.get():
+		
+			#Si l'utilisateur quitte, on met les variables 
+			#de boucle à 0 pour n'en parcourir aucune et fermer
+		if event.type == QUIT or event.type == KEYDOWN and event.key == K_ESCAPE:
+			continuer = 0
